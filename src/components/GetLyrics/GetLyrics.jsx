23d87d3cb5c75752787a9c3da149a18abe09f081
Petek Savaş Hamitbeyli <38165351+petekgithub@users.noreply.github.com>
2022-06-07 22:48:58 +0300

    showdata-and-getlyrics-components

--- src/components/GetLyrics/GetLyrics.jsx
+++ src/components/GetLyrics/GetLyrics.jsx
@@ -2,44 +2,38 @@ import React from 'react'
 
 const GetLyrics = () => {
   return (
-    <div>GetLyrics</div>
+    <>
+    
+    //  That part exactly the same with js code
+    async function getLyrics(artist, songTitle) {
+      const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+        const data = await res.json();
+    
+        const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+    
+        result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+        <span>${lyrics}</span>
+        `;
+    
+        more.innerHTML = '';
+    
+    }
+
+    result.addEventListener('click', e => {
+      const clickedEl = e.target;
+    
+      if(clickedEl.tagName === 'BUTTON') {
+        const artist = clickedEl.getAttribute('data-artist');
+        const songTitle= clickedEl.getAttribute('data-songtitle');
+    
+        getLyrics(artist, songTitle); 
+      }
+
+    });
+    
+
+    </>
   )
 }
 
 export default GetLyrics
-
-/**
- * 
- * // Get lyrics for songs
-async function getLyrics(artist, songTitle) {
-  const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
-    const data = await res.json();
-
-    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
-
-    result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
-    <span>${lyrics}</span>
-    `;
-
-    more.innerHTML = '';
-
-
-}
- * 
- */
-
-/**
- * 
- * // Get lyrics button click
-result.addEventListener('click', e => {
-  const clickedEl = e.target;
-
-  if(clickedEl.tagName === 'BUTTON') {
-    const artist = clickedEl.getAttribute('data-artist');
-    const songTitle= clickedEl.getAttribute('data-songtitle');
-
-    getLyrics(artist, songTitle); 
-
-  }
-});
- */
